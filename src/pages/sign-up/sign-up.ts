4ad715946f209db83db07cfc820b38ce6f979e81
Elio Briceño <eliobricenov@gmail.com>
2018-11-16 01:26:22 -0400

    added signup validation

--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { CustomValidators } from '../../util/validators/CustomValidators';
 
 /**
  * Generated class for the SignUpPage page.
@@ -15,11 +17,59 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignUpPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  reactiveForm: FormGroup;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private fb: FormBuilder) {
+    this.setReactiveForm();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SignUpPage');
+  private setReactiveForm(): void {
+    this.reactiveForm = this.fb.group({
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.email
+      ])],
+      security: this.fb.group({
+        password: ['', Validators.compose([
+          Validators.required,
+          Validators.pattern(CustomValidators.passwordRegex)
+        ])],
+        passwordConfirm: ['', Validators.required]
+      }, { validator: CustomValidators.passMatcher })
+    });
   }
+
+
+  get name(): AbstractControl {
+    return this.reactiveForm.get('name');
+  }
+
+  get lastName(): AbstractControl {
+    return this.reactiveForm.get('lastName');
+  }
+
+  get username(): AbstractControl {
+    return this.reactiveForm.get('username');
+  }
+
+  get security(): AbstractControl {
+    return this.reactiveForm.get('security');
+  }
+
+  get password(): AbstractControl {
+    return this.security.get('password');
+  }
+
+  get passwordConfirm(): AbstractControl {
+    return this.security.get('passwordConfirm');
+  }
+
+  get email(): AbstractControl {
+    return this.reactiveForm.get('email');
+  }
+
 
 }
