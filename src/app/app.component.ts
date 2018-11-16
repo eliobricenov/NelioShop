033b82f0741227a9361e45c119433dae553a32ca
Elio Briceño <eliobricenov@gmail.com>
2018-11-16 00:41:49 -0400

    added signup page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import { Storage } from '@ionic/storage';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: string = 'SignUpPage';
 
   pages: Array<{ title: string, component: any }>;
   user: any;
