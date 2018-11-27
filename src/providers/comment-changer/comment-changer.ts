a33f1ed5776663c9102f39fbd76adb835933484b
Nestor Amesty <namesty@cps.la>
2018-11-26 23:16:42 -0400

    Comments Started

---
+++ src/providers/comment-changer/comment-changer.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the CommentChangerProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class CommentChangerProvider {
+
+public comms:any = [];
+
+  constructor(public http: HttpClient) {
+    console.log('Hello CommentChangerProvider Provider');
+  }
+
+  findComm(id){
+    for(let i =0; i<this.comms.length;i++){
+      if(this.comms[i].id == id)
+      return i;
+    }
+    return -1
+   }
+
+   updateComm(d, i){
+    let index = this.findComm(i)
+    this.comms[index]=d;
+   }
+
+   deleteComm(i){
+    let index = this.findComm(i)
+    this.comms.splice(index, 1)
+   }
+
+   readComms(d){
+     this.comms = d;
+     console.log(this.comms)
+   }
+
+   createComm(d){
+     this.comms.push(d);
+   }
+
+   getByIndex(i){
+     return this.comms[i];
+   }
+
+}
